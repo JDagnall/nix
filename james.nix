8400eb7ec327986673347eba3c2145e63c526a5c
James Dagnall <james.t.dagnall@gmail.com>
2025-08-07 15:37:39 +1000

    fix shell not being installed

--- james.nix
+++ james.nix
@@ -7,9 +7,14 @@
     initialPassword = "pass";
     packages = with pkgs;
       [
-        # vim
+      	# things you always want installed no exceptions
+        vim
         git
+	zsh
       ];
+    # make sure we always have a shell
     shell = pkgs.zsh;
   };
+  # make sure we always have a shell
+  programs.zsh.enable = true;
 }
