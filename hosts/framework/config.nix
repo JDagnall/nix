3f75aa2bdcebb6f7c34d68e58255f7e174bdfdc8
James Dagnall <james.t.dagnall@gmail.com>
2025-08-07 18:48:12 +1000

    altered framework config

--- hosts/framework/config.nix
+++ hosts/framework/config.nix
@@ -8,8 +8,9 @@ pkgs, ... }:
   window-manager.hyprland.enable = true;
   boot-loader.grub.enable = true;
   fonts.enable = true;
-
   # config ------------------------------
+
+
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -34,14 +35,30 @@ pkgs, ... }:
   in formatted;
 
   time.timeZone = "Australia/Brisbane";
+  
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+  
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
     #useXkbConfig = true; # use xkb.options in tty.
   };
 
+  # use latest linux kernal
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   system.stateVersion = "25.05"; # Did you read the comment?
 
 }
