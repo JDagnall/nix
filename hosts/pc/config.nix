97e28defd06d3a5d1159093c237506f8cc48f4c0
James Dagnall <james.t.dagnall@gmail.com>
2025-10-21 20:24:20 +1000

    add host for pc

---
+++ hosts/pc/config.nix
@@ -0,0 +1,119 @@
+{
+	config,
+	# lib,
+	pkgs,
+	...
+}: {
+	# config ------------------------------
+	display-manager.gdm.enable = true;
+	window-manager.hyprland.enable = true;
+	boot-loader.grub.enable = true;
+	fonts.enable = true;
+
+	service.syncthing.enable = true;
+	service.syncthing.user = "james";
+	service.syncthing.group = "james";
+	service.syncthing.dataDir = "/home/james";
+	service.syncthing.configDir = "/home/james/.config/syncthing";
+	service.syncthing.devices.macmini-server.enable = true;
+	service.syncthing.folders = let
+		shareDevices = ["MacMini-Server"];
+	in {
+		secure.enable = true;
+		secure.share = shareDevices;
+		classes.enable = true;
+		classes.share = shareDevices;
+		proj.enable = true;
+		proj.share = shareDevices;
+		wallpapers.enable = true;
+		wallpapers.share = shareDevices;
+	};
+
+	service.docker.enable = true;
+	service.docker.groupUsers = ["james"];
+	service.openvpn3.enable = false;
+
+	stylix.enableConfig = true;
+	# config ------------------------------
+
+	imports = [
+		# Include the results of the hardware scan.
+		./hardware-configuration.nix
+		../../james.nix
+		../../modules
+		../../stylix.nix
+	];
+
+	environment.systemPackages = with pkgs; [home-manager];
+
+	nix.settings.experimental-features = [
+		"nix-command"
+		"flakes"
+	];
+
+	networking.networkmanager.enable = true;
+	networking.hostName = "pc";
+
+	services.openssh.enable = true;
+
+	services.pipewire.enable = true;
+
+	# bluetooth
+	hardware.bluetooth = {
+		enable = true;
+		powerOnBoot = true; # turn bt device on
+		settings = {
+			General = {
+				Experimental = true; # Enables showing battery of bt devices
+			};
+		};
+	};
+	services.blueman.enable = true; # bt manager software
+
+	services.flatpak.enable = true;
+
+	# garbage collector
+	nix.gc = {
+		automatic = true;
+		dates = "weekly";
+		persistent = true;
+		randomizedDelaySec = "10min";
+		options = "-d";
+	};
+
+	time.timeZone = "Australia/Brisbane";
+
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_GB.UTF-8";
+
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_AU.UTF-8";
+		LC_IDENTIFICATION = "en_AU.UTF-8";
+		LC_MEASUREMENT = "en_AU.UTF-8";
+		LC_MONETARY = "en_AU.UTF-8";
+		LC_NAME = "en_AU.UTF-8";
+		LC_NUMERIC = "en_AU.UTF-8";
+		LC_PAPER = "en_AU.UTF-8";
+		LC_TELEPHONE = "en_AU.UTF-8";
+		LC_TIME = "en_AU.UTF-8";
+	};
+
+	console = {
+		font = "Lat2-Terminus16";
+		keyMap = "us";
+		#useXkbConfig = true; # use xkb.options in tty.
+	};
+
+	# use latest linux kernal
+	boot.kernelPackages = pkgs.linuxPackages_latest;
+
+	# adds list of currently used system packages to /etc/nixos/current-system-packages
+	environment.etc."current-system-packages".text = let
+		packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+		sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
+		formatted = builtins.concatStringsSep "\n" sortedUnique;
+	in
+		formatted;
+
+	system.stateVersion = "25.05"; # Did you read the comment?
+}
---
+++ hosts/pc/home.nix
@@ -0,0 +1,79 @@
+{
+	pkgs,
+	config,
+	...
+}: {
+	home.username = "james";
+	home.homeDirectory = "/home/james";
+
+	home.stateVersion = "25.05"; # Please read the comment before changing.
+
+	imports = [
+		../../home-modules
+	];
+
+	### Configs
+	shell.zsh.enable = true;
+	shell.zsh.dircolors.enable = true;
+	shell.zsh.direnv.enable = true;
+	shell.bat.enable = true;
+	shell.eza.enable = true;
+	shell.fzf.enable = true;
+	shell.zoxide.enable = true;
+
+	langs.nix.enable = true;
+	langs.python.enable = true;
+
+	window-manager.hyprland.enable = true;
+
+	term.wezterm.enable = true;
+
+	ui.rofi.enable = true;
+	ui.waybar.enable = true;
+	ui.hyprpaper.enable = true;
+	ui.hyprlock.enable = true;
+	ui.hypridle.enable = true;
+	ui.firefox.enable = true;
+	ui.nwg-look.enable = false;
+	ui.syncthingtray.enable = true;
+	ui.spotify.enable = true;
+	ui.slack.enable = true;
+	ui.legcord.enable = true;
+	ui.mako.enable = true;
+	ui.swayosd.enable = true;
+	ui.nm-applet.enable = true;
+	ui.bt-applet.enable = true;
+	ui.vscode.enable = true;
+	ui.obsidian.enable = true;
+	ui.thunar.enable = true;
+	ui.imv.enable = true;
+	ui.hypr-screenshot.enable = true;
+	ui.easyeffects.enable = true;
+	ui.pavucontrol.enable = true;
+
+	tools.git.enable = true;
+	tools.pipenv.enable = true;
+	tools.mycli.enable = true;
+	tools.keepassxc.enable = true;
+	tools.keepmenu.enable = true;
+	tools.gh-cli.enable = true;
+	tools.nh.enable = true;
+
+	nixLoki.enable = true;
+
+	stylix.enableHomeConfig = true; # home-manager specific stylix
+
+	### Configs
+
+	# installed packages
+	home.packages = [];
+
+	home.file = {
+		home_packages.text = let
+			packages = builtins.map (p: "${p.name}") config.home.packages;
+			sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
+			formatted = builtins.concatStringsSep "\n" sortedUnique;
+		in
+			formatted;
+	};
+}
