0f77ac86b159148b2ec92e57bb79382620ba0e8b
James Dagnall <james.t.dagnall@gmail.com>
2025-10-30 05:21:07 +1000

    pc | gen: 32 version: 25.11.20250902.d0fc308 | Thu 30 Oct 2025 05:21:07 AEST

--- home-modules/gaming/mangohud.nix
+++ home-modules/gaming/mangohud.nix
@@ -15,9 +15,26 @@
 			programs.mangohud = {
 				enable = true;
 				enableSessionWide = true;
-				settings = {
-					legacy_layout = 2;
-				};
+				# override some of stylix's bad decisions
+				settings =
+					lib.mkForce {
+						# legacy_layout = 2;
+						gpu_name = true;
+						gpu_temp = true;
+						cpu_temp = true;
+						cpu_load = true;
+						vram = true;
+						ram = true;
+						fps = true;
+						wine = true;
+						gamemode = true;
+						graphs = "";
+
+						round_corners = 0.5;
+						horizontal = true;
+						background_alpha = 0.5;
+						alpha = 0.8;
+					};
 				settingsPerApplication = {};
 			};
 			stylix.targets.mangohud.enable = config.stylix.enableHomeConfig;
