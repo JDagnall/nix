4e32ced9435dfb0a5b3ce339bad6bda1b43dd4ce
James Dagnall <james.t.dagnall@gmail.com>
2026-03-31 20:00:40 +1000

    pc | gen: 242 version: 26.05.20260324.46db2e0 | Tue 31 Mar 2026 20:00:40 AEST

--- modules/gaming/gamemode.nix
+++ modules/gaming/gamemode.nix
@@ -16,6 +16,10 @@
 				enable = true;
 				enableRenice = true;
 				settings = {
+					custom = {
+						start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+						end = "${pkgs.libnotify}/bin/notify-send 'GameMode exited'";
+					};
 				};
 			};
 		};
